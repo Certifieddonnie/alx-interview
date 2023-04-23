4933a10230d4a0c32858f7a8884e2741684c57ec
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-04-23 20:38:20 +0100

    docs and lint styling

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,27 +2,29 @@
 # This Script solves the pascal principle
 
 def factorial(num):
+    """ Factorial Function """
     if (num == 1) or (num == 0):
         return 1
-    return (num * factorial(num-1)) # multiplication
+    return (num * factorial(num-1))  # multiplication
 
-def pascal_triangle(n): # n = number
-    triangle = [] # empty list
-    if (n <= 0): # conditional statement for number less than 1
+
+def pascal_triangle(n):  # n = number
+    """ Pascal function """
+    triangle = []  # empty list
+    if (n <= 0):  # conditional statement for number less than 1
         return triangle
-    for x in range(0, n): # iterate through 0 to (number-1) x is row number
-        temp = [] # temporary empty list
-        for y in range(0, x+1): # iterate through 0 to (x+1) y is column number
+    for x in range(0, n):  # iterate through 0 to (number-1) x is row number
+        temp = []  # temporary empty list
+        for y in range(0, x+1):  # iterate through 0 to(x+1) y is column number
             # print(x,y)
-            temp.append(Combination(x, y)) # called the combination function
-            
-        
+            temp.append(Combination(x, y))  # called the combination function
         triangle.append(temp)
-    
+
     return (triangle)
 
 
 def Combination(x, y):
+    """ Combination FUnction """
     # print("I am base fact",factorial(y) * factorial(x-y))
     return int(factorial(x)/(factorial(y)*factorial(x-y)))
 
