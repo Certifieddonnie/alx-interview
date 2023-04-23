c4ae1803ad3f96a7b1f8e08f031f6e7db25c3882
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-04-23 20:53:20 +0100

    bug fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,9 +11,9 @@ def factorial(num):
 
 def pascal_triangle(n):  # n = number
     """ Pascal function """
-    triangle = [[]]  # empty list
+    triangle = []  # empty list
     if (n <= 0):  # conditional statement for number less than 1
-        return triangle
+        return [[]]
     for x in range(0, n):  # iterate through 0 to (number-1) x is row number
         temp = []  # temporary empty list
         for y in range(0, x+1):  # iterate through 0 to(x+1) y is column number
